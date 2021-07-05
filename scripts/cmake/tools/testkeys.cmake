6a2361de68e30897c9189ac4a1a9eb109bb9af5c
Miodrag Milanovic <mmicko@gmail.com>
2021-07-05 21:01:51 +0200

    fix testkeys compile

--- scripts/cmake/tools/testkeys.cmake
+++ scripts/cmake/tools/testkeys.cmake
@@ -11,8 +11,12 @@ target_include_directories(testkeys PRIVATE
 	${MAME_DIR}/src/osd
 	${MAME_DIR}/src/lib/util
 )
+if (MINGW)
+	target_link_libraries(testkeys PRIVATE mingw32)
+	target_link_libraries(testkeys PRIVATE SDL2::SDL2main)
+endif()
 
-target_link_libraries(testkeys PRIVATE utils)
+target_link_libraries(testkeys PRIVATE ocore_sdl)
 
 target_sources(testkeys PRIVATE
 	${MAME_DIR}/src/tools/testkeys.cpp
